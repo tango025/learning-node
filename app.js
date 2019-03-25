81fe36f88ac9bea780055f73c232f97d19ce53bb
tango025 <gaurav2550.gg@gmail.com>
2019-03-25 20:14:29 +0530

    notes app beginning

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
-const notes = require('./notes.js')
-// const user = os.userInfo();
-// fs.appendFile('greeting.txt', `hello ${user.username} ! `, function (err) {
-//     if (err) console.log('error');
-// })
 
-console.log(_.isString(2));
-console.log(_.uniq(['a','b','b',2,2]));
-console.log(notes.addNote()); 
+const notes = require('./notes.js')
+
+const argument = process.argv[2];
+
+if(argument === 'read'){ console.log('read');}
+else if(argument === 'list'){ console.log('list');}
+else if(argument === 'add') {console.log('add');}
+else if (argument === 'remove') { console.log('remove'); }
+
+else {console.log('wrong command');}
